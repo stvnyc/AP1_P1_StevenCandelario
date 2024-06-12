4d423bf3e8d620cfce11151b63fd6be8bfb26d4a
Steven Candelario <stevencandelariof@gmail.com>
2024-06-12 18:58:09 -0400

    Agregando contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AP1_P1_StevenCandelario.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AP1_P1_StevenCandelario.DAL;
 
 public class Contexto : DbContext
 {
+	public Contexto(DbContextOptions<Contexto> options)
+		: base(options) { }
 
+	public DbSet<Articulos> Articulos { get; set; }
 }
