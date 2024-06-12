db862efb4bc5c0d07b5657054a73979f76901399
Steven Candelario <stevencandelariof@gmail.com>
2024-06-12 18:54:41 -0400

    Sustituyendo letras sin tilde

--- Models/Articulos.cs
+++ Models/Articulos.cs
@@ -7,16 +7,16 @@ public class Articulos
 	[Key]
 	public int ArticuloId {  get; set; }
 
-	[Required(ErrorMessage = "El campo Descripcion es obligatorio")]
-	[RegularExpression(@"^[a-zA-ZñÑ\s]+$", ErrorMessage ="La descripcion no debe contener caracteres especiales")]
+	[Required(ErrorMessage = "El campo Descripción es obligatorio")]
+	[RegularExpression(@"^[a-zA-ZñÑ\s]+$", ErrorMessage = "La descripción no debe contener caracteres especiales")]
 	public string? Descripcion { get; set; }
 
 	[Required(ErrorMessage = "El campo PctItbis es obligatorio")]
-	[Range (0, 18, ErrorMessage = "Ingrese un numero entre 0 y 18")]
+	[Range (0, 18, ErrorMessage = "Ingrese un número entre 0 y 18")]
 	public decimal PctItbis { get; set; }
 	
 	[Required(ErrorMessage = "El campo Costo es obligatorio")]
-	[Range (0.01, 20000000, ErrorMessage = "Ingrese un numero mayor a 0")]
+	[Range (0.01, 20000000, ErrorMessage = "Ingrese un número mayor a 0")]
 	public decimal Costo { get; set; }
 	public decimal Itbis { get; set; }
 }
